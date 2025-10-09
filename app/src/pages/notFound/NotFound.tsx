ea958648758fa586dd4134c5e4de6bcd82d3c4b3
ArthurAngeloFollador <arthurfollador99@gmail.com>
2025-10-09 10:51:41 -0300

    feat: not found page

--- app/src/pages/notFound/NotFound.tsx
+++ app/src/pages/notFound/NotFound.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import { GoAlert } from "react-icons/go";
+import * as S from "./notFoundStyle";
+import Buttons from "../../components/buttons/ButtonStyles";
+import { useNavigate } from "react-router-dom";
 
 function NotFound() {
-
-    return (
-        <>not found</>
-    )
+    const navigate = useNavigate();
+  return (
+    <>
+      <S.NotFoundContainer>
+        <GoAlert color="#3b82f6" size={120} />
+        <S.NotFoundTitle>404</S.NotFoundTitle>
+        <S.NotFoundSubText>Page Not Found</S.NotFoundSubText>
+        <S.Message>Oops! The page you are looking for doesn't exist. It might have been moved or deleted.</S.Message>
+        <Buttons.Large onClick={() => navigate("/dashboard")}>Back to Dashboard</Buttons.Large>
+      </S.NotFoundContainer>
+    </>
+  );
 }
 
-export default NotFound
+export default NotFound;
